52d49b5bcc9c0285ad0623c4eeff37a88999d2f5
주재민 <jujaemin@jujaemin-ui-MacBookAir.local>
2023-05-02 16:13:58 +0900

    Header 임시

---
+++ frontend/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Container = styled.header`
+  width: 100%;
+  height: 120px;
+  position: fixed;
+  background-color: #0d630d;
+`;
+
+export default function Header() {
+  return <Container></Container>;
+}
