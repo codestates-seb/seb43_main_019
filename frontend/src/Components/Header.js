9315cbb7b3a5e76694cdad58cb82b61745309e88
HwanTech <78897615+wjdghksdigh@users.noreply.github.com>
2023-05-03 16:28:59 +0900

    style : 헤더 로고 추가

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,12 +1,22 @@
 import styled from "styled-components";
 import { CommonButton } from "./Button";
 import { Link } from "react-router-dom";
+import Logo from '../assets/Images/logo.png';
 
 const Container = styled.header`
   width: 100%;
   height: 120px;
   position: fixed;
   background-color: var(--emerald-700);
+
+  .logo-container {
+    margin-left: 43%;
+    margin-top: 40px;
+    bottom: 8px;
+    content: '';
+    position: absolute;
+    top: 8px;
+  }
 
   .button-container {
     padding-left: 1200px; // 임시
@@ -15,9 +25,19 @@ const Container = styled.header`
     }
 `;
 
+const LogoImg = styled.img`
+        display: block;
+        width: 150px;
+        height: 30px;
+        margin-top: -4px;
+`
+
 export default function Header() {
   return(
    <Container>
+    <div className="logo-container">
+    <LogoImg src={Logo} alt="logo" />
+    </div>
     <Link to="/Mypage">
     <div className="button-container">
       <CommonButton>MyPage</CommonButton>  
