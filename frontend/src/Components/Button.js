7aebb615ad778b00943fe76eae6723614176d4e5
HwanTech <78897615+wjdghksdigh@users.noreply.github.com>
2023-05-03 15:22:56 +0900

    Design : 버튼 css , 위치 수정

--- frontend/src/Components/Button.js
+++ frontend/src/Components/Button.js
@@ -1,67 +1,20 @@
 import styled from "styled-components";
 
 export const CommonButton = styled.button`
-  display: inline-block;
-  transition: all 0.2s ease-in;
-  position: relative;
-  overflow: hidden;
-  z-index: 1;
-  color: #090909;
-  padding: 0.7em 1.7em;
-  font-size: 18px;
-  border-radius: 0.5em;
-  background: #e8e8e8;
-  border: 1px solid #e8e8e8;
-  box-shadow: 6px 6px 12px #c5c5c5,
-             -6px -6px 12px #ffffff;
- 
-  &:active{
-  color: #666;
-  box-shadow: inset 4px 4px 12px #c5c5c5,
-             inset -4px -4px 12px #ffffff;
-  }
-  &::before{
-  content: "";
-  position: absolute;
-  left: 50%;
-  transform: translateX(-50%) scaleY(1) scaleX(1.25);
-  top: 100%;
-  width: 140%;
-  height: 180%;
-  background-color: rgba(0, 0, 0, 0.05);
-  border-radius: 50%;
-  display: block;
-  transition: all 0.5s 0.1s cubic-bezier(0.55, 0, 0.1, 1);
-  z-index: -1;
-  }
-  &::after{
-  content: "";
-  position: absolute;
-  left: 55%;
-  transform: translateX(-50%) scaleY(1) scaleX(1.45);
-  top: 180%;
-  width: 160%;
-  height: 190%;
-  background-color: #009087;
-  border-radius: 50%;
-  display: block;
-  transition: all 0.5s 0.1s cubic-bezier(0.55, 0, 0.1, 1);
-  z-index: -1;
-  }
-  &:hover{
-  color: #ffffff;
-  border: 1px solid #009087;
-  }
-  &:hover::before{
-  top: -35%;
-  background-color: #009087;
-  transform: translateX(-50%) scaleY(1.3) scaleX(0.8);
-  }
-  &:hover::after{
-  top: -45%;
-  background-color: #009087;
-  transform: translateX(-50%) scaleY(1.3) scaleX(0.8);
-  }
+  /* padding: 1.3em 3em; */
+  font-size: 12px;
+  text-transform: uppercase;
+  letter-spacing: 2.5px;
+  font-weight: 500;
+  color: var(--white);
+  background-color: var(--emerald-700);
+  border: none;
+  border-radius: 45px;
+  /* box-shadow: 0px 8px 15px rgba(0, 0, 0, 0); */
+  /* transition: all 0.3s ease 0s; */
+  /* cursor: pointer; */
+  /* outline: none; */
+
 `;
 
 // 사용예시
--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,12 +1,25 @@
 import styled from "styled-components";
+import { CommonButton } from "./Button";
 
 const Container = styled.header`
   width: 100%;
   height: 120px;
   position: fixed;
   background-color: var(--emerald-700);
+
+  .button-container {
+    padding-left: 1200px; // 임시
+    padding-top : 90px; // 임시
+      display: flex;
+    }
 `;
 
 export default function Header() {
-  return <Container></Container>;
+  return(
+   <Container>
+    <div className="button-container">
+      <CommonButton>MyPage</CommonButton>
+    </div>
+  </Container>
+  );
 }
