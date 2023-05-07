37f8fbabf0569783d366fa1d1f072c2399b0eebd
exertivestar <exertivestar@naver.com>
2023-05-08 02:08:27 +0900

    Feat: exception 패키지 구현

---
+++ backend/aircamp/src/main/java/com/osdoor/aircamp/exception/BusinessLogicException.java
@@ -0,0 +1,13 @@
+package com.osdoor.aircamp.exception;
+
+import lombok.Getter;
+
+public class BusinessLogicException extends RuntimeException {
+    @Getter
+    private ExceptionCode exceptionCode;
+
+    public BusinessLogicException(ExceptionCode exceptionCode) {
+        super(exceptionCode.getMessage());
+        this.exceptionCode = exceptionCode;
+    }
+}
---
+++ backend/aircamp/src/main/java/com/osdoor/aircamp/exception/ExceptionCode.java
@@ -0,0 +1,26 @@
+package com.osdoor.aircamp.exception;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+    MEMBER_NOT_FOUND(404, "회원을 찾을 수 없습니다."),
+    MEMBER_EXISTS(409, "회원이 존재합니다."),
+    COFFEE_NOT_FOUND(404, "상품을 찾을 수 없습니다."),
+    COFFEE_CODE_EXISTS(409, "상품 코드가 존재합니다."),
+    ORDER_NOT_FOUND(404, "에약을 찾을 수 없습니다."),
+    CANNOT_CANCEL_ORDER(403, "이미 취소된 예약입니다."),
+    NOT_IMPLEMENTATION(501, "해당 기능을 수행할 수 없거나 아직 구현되지 않았습니다."),
+    INVALID_MEMBER_STATUS(400, "유효하지 않은 회원 상태입니다.");
+
+    @Getter
+    private int status;
+
+    @Getter
+    private String message;
+
+    ExceptionCode(int code, String message) {
+        this.status = code;
+        this.message = message;
+    }
+}
+
