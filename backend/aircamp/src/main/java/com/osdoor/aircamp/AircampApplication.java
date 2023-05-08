802778505da8c81322351705e8730a931946dd0e
hyunwoojo <hwchokim95@hanmail.net>
2023-05-08 11:01:17 +0900

    FEAT_hw_ AircampApplication : audit 기능 적용을 위해, @EnableJpaAuditing 추가

--- backend/aircamp/src/main/java/com/osdoor/aircamp/AircampApplication.java
+++ backend/aircamp/src/main/java/com/osdoor/aircamp/AircampApplication.java
@@ -2,8 +2,10 @@ package com.osdoor.aircamp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJㄷpaAuditing
 public class AircampApplication {
 
 	public static void main(String[] args) {
