a69d1be462c6e08ff5bb311b04946f0545793062
hyunwoojo <hwchokim95@hanmail.net>
2023-05-08 11:03:26 +0900

    FIX_hw_ AircampApplication : @EnableJpaAuditing 오타 수정

--- backend/aircamp/src/main/java/com/osdoor/aircamp/AircampApplication.java
+++ backend/aircamp/src/main/java/com/osdoor/aircamp/AircampApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJㄷpaAuditing
+@EnableJpaAuditing
 public class AircampApplication {
 
 	public static void main(String[] args) {
