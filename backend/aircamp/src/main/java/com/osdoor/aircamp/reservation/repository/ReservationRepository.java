f2843b87398a7378891aa8c4a94a52566933fe88
exertivestar <120434232+exertivestar@users.noreply.github.com>
2023-05-08 10:48:00 +0900

    Revert "Feat/reservation hb"

--- backend/aircamp/src/main/java/com/osdoor/aircamp/reservation/repository/ReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.osdoor.aircamp.reservation.repository;
-
-import com.osdoor.aircamp.reservation.entity.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-}
