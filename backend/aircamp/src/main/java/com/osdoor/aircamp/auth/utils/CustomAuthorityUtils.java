f829d10c1555b4e6a091a5bb9bdd4c841284f185
SHyeonCoding <tkdgus3365@gmail.com>
2023-05-09 23:56:04 +0900

    FEAT: SELLER 권한 추가

--- backend/aircamp/src/main/java/com/osdoor/aircamp/auth/utils/CustomAuthorityUtils.java
+++ backend/aircamp/src/main/java/com/osdoor/aircamp/auth/utils/CustomAuthorityUtils.java
@@ -16,8 +16,10 @@ public class CustomAuthorityUtils {
     private String adminMailAddress;
 
     private final List<GrantedAuthority> ADMIN_ROLES = AuthorityUtils.createAuthorityList("ROLE_ADMIN", "ROLE_USER");
+    private final List<GrantedAuthority> SELLER_ROLES = AuthorityUtils.createAuthorityList("ROLE_SELLER", "ROLE_USER");
     private final List<GrantedAuthority> USER_ROLES = AuthorityUtils.createAuthorityList("ROLE_USER");
     private final List<String> ADMIN_ROLES_STRING = List.of("ADMIN", "USER");
+    private final List<String> SELLER_ROLES_STRING = List.of("SELLER", "USER");
     private final List<String> USER_ROLES_STRING = List.of("USER");
 
     // 메모리 상의 Role을 기반으로 권한 정보 생성.
@@ -40,6 +42,13 @@ public class CustomAuthorityUtils {
     public List<String> createRoles(String email) {
         if (email.equals(adminMailAddress)) {
             return ADMIN_ROLES_STRING;
+        }
+        return USER_ROLES_STRING;
+    }
+
+    public List<String> createRoles(boolean isSeller) {
+        if(isSeller) {
+            return SELLER_ROLES_STRING;
         }
         return USER_ROLES_STRING;
     }
