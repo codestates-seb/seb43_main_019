49180b3a744fcac424f1d662993782779784932e
hyunwoojo <hwchokim95@hanmail.net>
2023-05-11 22:46:12 +0900

    FEAT_hw_ 탈퇴한 회원을 재가입 시킬 수 있는 api 작성

--- backend/aircamp/src/main/java/com/osdoor/aircamp/member/mapper/MemberMapper.java
+++ backend/aircamp/src/main/java/com/osdoor/aircamp/member/mapper/MemberMapper.java
@@ -3,6 +3,7 @@ package com.osdoor.aircamp.member.mapper;
 import com.osdoor.aircamp.member.dto.MemberPatchDto;
 import com.osdoor.aircamp.member.dto.MemberPostDto;
 import com.osdoor.aircamp.member.dto.MemberResponseDto;
+import com.osdoor.aircamp.member.dto.ReSignUpDto;
 import com.osdoor.aircamp.member.entity.Member;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
@@ -14,6 +15,12 @@ public interface MemberMapper {
 
     Member memberPostDtoToMember(MemberPostDto memberDto);
     Member memberPatchDtoToMember(MemberPatchDto memberDto);
+    default Member reSighupDtoToMember(ReSignUpDto reSignUpDto) {
+        Member member = new Member();
+        member.setMemberId(reSignUpDto.getMemberId());
+        member.setMemberStatus(reSignUpDto.getMemberStatus());
+        return member;
+    }
     MemberResponseDto memberToMemberResponseDto(Member member);
     List<MemberResponseDto> memberToMemberResponseDtos(List<Member> members);
 }
