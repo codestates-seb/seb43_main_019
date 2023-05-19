0ad1d80b116e8546ae3ed3a9f6fdc943adb2cab3
주재민 <jujaemin@jujaemin-ui-MacBookAir.local>
2023-05-19 11:16:18 +0900

    Feat: 카카오 로그인 완료

--- frontend/src/Pages/KakaoLogin.jsx
+++ frontend/src/Pages/KakaoLogin.jsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 import axios from "axios";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { handleLogin } from "../Redux/Actions";
 import { handleKakaoLogin } from "../utils/MemberFunctions";
 
@@ -18,23 +18,43 @@ const Wrapper = styled.div`
 const Loader = styled.h1``;
 
 export default function KakaoLogin() {
+  const location = useLocation();
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const location = useLocation();
-  const KAKAO_CODE = location.search.split("=")[1];
-
-  const getKakaoToken = async () => {
-    const userInfo = await handleKakaoLogin(KAKAO_CODE);
-
-    // dispatch(handleLogin(userInfo));
-    navigate("/");
-  };
-
   useEffect(() => {
-    if (!location.search) return;
+    const searchParams = new URLSearchParams(location.search);
 
-    getKakaoToken();
+    const accessToken = searchParams.get("accessToken");
+    const refreshToken = searchParams.get("refreshToken");
+    console.log(accessToken);
+
+    const validTokens = accessToken.split(".");
+
+    const encodedUserInfo = validTokens[1];
+
+    const sanitizedString = encodedUserInfo
+      .replace(/-/g, "+")
+      .replace(/_/g, "/");
+
+    const decodedUserInfo = decodeURIComponent(
+      Array.prototype.map
+        .call(atob(sanitizedString), function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+
+    const decodedObject = JSON.parse(decodedUserInfo);
+
+    const user = {
+      memberId: decodedObject.memberId,
+      nickname: decodedObject.nickname,
+      email: decodedObject.nickname,
+    };
+
+    dispatch(handleLogin(user));
+    navigate("/");
   }, []);
 
   return (
