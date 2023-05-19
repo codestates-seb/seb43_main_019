1b2c4de2ba2d283882b1dbdb43d068e275b4156f
주재민 <jujaemin@jujaemin-ui-MacBookAir.local>
2023-05-19 21:59:40 +0900

    Remove: test-server 삭제

--- frontend/test_server/node_modules/@aws-sdk/client-s3/dist-types/ts3.4/pagination/Interfaces.d.ts
+++
@@ -1,5 +0,0 @@
-import { PaginationConfiguration } from "@aws-sdk/types";
-import { S3Client } from "../S3Client";
-export interface S3PaginationConfiguration extends PaginationConfiguration {
-  client: S3Client;
-}
